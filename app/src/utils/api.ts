533a4f31a816dbc1f959be9fa01f9df7d3c02462
n-oshiumi <igbmh.yz.way2@gmail.com>
2023-05-17 23:41:38 +0900

    バックエンドエンドポイントを.envで指定できるようにした

--- app/src/utils/api.ts
+++ app/src/utils/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 const api = axios.create({
-  baseURL: "http://localhost:85",
+  baseURL: API_BASE_URL,
   headers: {
     Accept: "application/json",
   },
